ad203c36dc86e60a0642fd7d0dae7479ec3936f1
AntonMikhailov1 <mikhailovanton.nsk@gmail.com>
2023-09-30 23:42:48 +0300

    fix: add helmet, add auth to routers

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const httpStatus = require('http-status-codes').StatusCodes;
 const CardsRouter = require('./routes/cards');
 const UsersRouter = require('./routes/users');
+const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
+const { validateUser, validateLogin } = require('./middlewares/validation');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -17,13 +22,13 @@ app.get('/', (req, res) => {
   res.status(httpStatus.OK).send('Hello World!');
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateUser, createUser);
 
-app.use(CardsRouter);
-app.use(UsersRouter);
+app.use('/', auth, CardsRouter);
+app.use('/', auth, UsersRouter);
 
-app.use('/*', (req, res) => {
+app.use('/*', (res) => {
   res.status(httpStatus.NOT_FOUND).send({ message: 'Страница не найдена' });
 });
 
